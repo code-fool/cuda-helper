d9926c257ef7efd6d8e80759e4b81c6b44b2223b
code-fool <wangze@web3.com>
2022-12-29 08:58:39 +0800

    test first cmake change

--- check.sh
+++ check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 BUILD_DIR="build-release"
 mkdir -p $BUILD_DIR
-cmake -DCUDA_HELPER_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Release -S . -B $BUILD_DIR
+# cmake -DCUDA_HELPER_BUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Release -S . -B $BUILD_DIR
+# cmake --build $BUILD_DIR --clean-first
+
+cmake -S . -B $BUILD_DIR -DCMAKE_CXX_COMPILER=/opt/rocm-5.4.0/llvm/bin/clang++ -DCMAKE_BUILD_TYPE=release -DCUDA_HELPER_BUILD_TESTS=ON 
 cmake --build $BUILD_DIR --clean-first
+
 cd $BUILD_DIR
-./cuda-helper-test
+./blake3_cuda-test
